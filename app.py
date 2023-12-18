a61dc25b7d66308edcb8e5cd1902ba653c0f13d1
Md.Azizul Hakim <145244208+DreamIsMl@users.noreply.github.com>
2023-12-18 16:52:44 +0600

    app.py

--- app.py
+++ app.py
@@ -1,41 +1,31 @@
 import pickle
-
 import pandas as pd
 import streamlit as st
+import requests  # Add this import for handling HTTP requests
 
-teams = ['South Africa',
- 'India',
- 'New Zealand',
- 'West Indies',
- 'Australia',
- 'Sri Lanka',
- 'England',
- 'Bangladesh',
- 'Pakistan']
+teams = ['South Africa', 'India', 'New Zealand', 'West Indies', 'Australia', 'Sri Lanka', 'England', 'Bangladesh', 'Pakistan']
 
-cities = ['Sharjah Cricket Stadium', 'Saxton Oval', 'Hyderabad',
-       'Sydney Cricket Ground', 'Abu Dhabi', 'Harare Sports Club',
-       'Nagpur', 'The Rose Bowl', 'Visakhapatnam', 'Headingley',
-       'Premadasa Stadium', 'Dubai', 'Dharamsala',
-       'New Wanderers Stadium', 'Queen Park Oval', 'Kennington Oval',
-       'University Oval', 'Kimberley', 'Ahmedabad', 'Ranchi', 'Malahide',
-       'Senwes Park', 'Delhi', 'Shere Bangla', 'Boland Park', 'Bay Oval']
+cities = ['Sharjah Cricket Stadium', 'Saxton Oval', 'Hyderabad', 'Sydney Cricket Ground', 'Abu Dhabi', 'Harare Sports Club', 'Nagpur', 'The Rose Bowl', 'Visakhapatnam', 'Headingley', 'Premadasa Stadium', 'Dubai', 'Dharamsala', 'New Wanderers Stadium', 'Queen Park Oval', 'Kennington Oval', 'University Oval', 'Kimberley', 'Ahmedabad', 'Ranchi', 'Malahide', 'Senwes Park', 'Delhi', 'Shere Bangla', 'Boland Park', 'Bay Oval']
 
-pipe = pickle.load(open('F:\\class\\Machine Learning\\project\\T20 Match Win Probalility\\model.pkl','rb'))
+# Load the model from GitHub
+model_url = 'https://raw.githubusercontent.com/DreamIsMl/T20-Match-Win-Probability/edit/master/model.pkl'
+response = requests.get(model_url)
+pipe = pickle.loads(response.content)
+
 st.title('T20 Match Win Predictor')
 
 col1, col2 = st.columns(2)
 
 with col1:
-    batting_team = st.selectbox('Select the batting team',sorted(teams))
+    batting_team = st.selectbox('Select the batting team', sorted(teams))
 with col2:
-    bowling_team = st.selectbox('Select the bowling team',sorted(teams))
+    bowling_team = st.selectbox('Select the bowling team', sorted(teams))
 
-selected_city = st.selectbox('Select host city',sorted(cities))
+selected_city = st.selectbox('Select host city', sorted(cities))
 
 target = st.number_input('Target')
 
-col3,col4,col5 = st.columns(3)
+col3, col4, col5 = st.columns(3)
 
 with col3:
     score = st.number_input('Score')
@@ -46,15 +36,17 @@ with col5:
 
 if st.button('Predict Probability'):
     runs_left = target - score
-    ball_left = 120 - (overs*6)
+    ball_left = 120 - (overs * 6)
     wickets = 10 - wickets
-    crr = score/overs
-    rrr = (runs_left*6)/ball_left
+    crr = score / overs
+    rrr = (runs_left * 6) / ball_left
 
-    input_df = pd.DataFrame({'batting_team':[batting_team],'bowling_team':[bowling_team],'city':[selected_city],'runs_lef':[runs_left],'ball_left':[ball_left],'wickets':[wickets],'total_runs_x':[target],'crr':[crr],'rrr':[rrr]})
+    input_df = pd.DataFrame(
+        {'batting_team': [batting_team], 'bowling_team': [bowling_team], 'city': [selected_city], 'runs_lef': [runs_left],
+         'ball_left': [ball_left], 'wickets': [wickets], 'total_runs_x': [target], 'crr': [crr], 'rrr': [rrr]})
 
     result = pipe.predict_proba(input_df)
     loss = result[0][0]
     win = result[0][1]
-    st.header(batting_team + "- " + str(round(win*100)) + "%")
-    st.header(bowling_team + "- " + str(round(loss*100)) + "%")
+    st.header(batting_team + "- " + str(round(win * 100)) + "%")
+    st.header(bowling_team + "- " + str(round(loss * 100)) + "%")
